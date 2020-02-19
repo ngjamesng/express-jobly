c62568ec2dbf89dea3ec568b8727d5e2ba925759
moogiecodes <mchoichoi@gmail.com>
2020-02-19 12:04:39 -0800

    Companies routes: post, get, patch [IP]

--- models/company.js
+++ models/company.js
@@ -1,25 +1,67 @@
 const db = require("../db");
-
+const sqlForPartialUpdate = require("..helpers/partialUpdate");
 const expressError = require("../helpers/expressError");
 
 class Company {
-	/**
- * CRUD for companies 
- */
+  /**
+   * CRUD for companies 
+   */
 
   /**
    * 
     search takes in a term, and optional min and max employees. 
    */
-	static async search(searchTerm, min_employees = 0, max_employees = Infinity) {
-		let q = `%${searchTerm}%`;
-		const results = await db.query(
-			`SELECT handle, name 
-    FROM companies 
-    WHERE LOWER(handle) ILIKE $1 
-    OR LOWER(name) ILIKE $1`,
-			[ q, min_employees, max_employees ]
+  static async search(searchTerm, min_employees = 0, max_employees = Infinity) {
+    let q = `%${searchTerm}%`;
+    const results = await db.query(
+      `SELECT handle, name 
+      FROM companies 
+      WHERE LOWER(handle) ILIKE $1 
+      OR LOWER(name) ILIKE $1
+      FILTER WHERE(num_employees >= $2 AND num_employees <= $3)`,
+      [q, min_employees, max_employees]
     );
-    return results
-	}
+    return results.rows;
+  }
+
+  /** Create new company */
+  static async create(handle, name, num_employees, description, logo_url) {
+    const results = await db.query(
+      `INSERT INTO companies 
+        (handle, name, num_employees, description, logo_url)
+        VALUES ($1, $2, $3, $4, $5)
+        RETURNING handle, name, num_employees, description, logo_url`,
+      [handle, name, num_employees, description, logo_url]
+    );
+
+    return results.rows[0];
+  }
+
+  /** Get a company by HANDLE */
+  static async get(handle) {
+    const result = await db.query(
+      `SELECT handle, name, num_employees, description, logo_url 
+        FROM companies
+        WHERE handle = $1`,
+      [handle]
+    );
+
+    return result.rows[0];
+  }
+
+  /** Update exisiting company by handle */
+  static async update(handle, body) {
+    const { handle: newHandle, name, num_employees, description, logo_url } = body;
+    const items = {
+      handle: newHandle,
+      name,
+      num_employees,
+      description,
+      logo_url
+    }
+    // KEY AND ID
+    const result = sqlForPartialUpdate("companies", items, key, id)
+
+  }
+
 }
--- routes/companies.js
+++ routes/companies.js
@@ -9,13 +9,64 @@ const router = new express.Router();
 modules.exports = router;
 
 //returns a list of companies => {companies: [companyData, ...]}
-router.get("/", async function(req, res, next) {
-	try {
-    let { search: searchTerm, min_employees, max_employees } = req.body;
-    if(min_employees > max_employees) throw new ExpressError("min_employees cannot be greater than max_employees", 400)
-    let companies = await Companies.search(searchTerm, max_employees, min_employees);
-    return res.json(companies);
-	} catch (err) {
-		return next(err);
-	}
+router.get("/", async function (req, res, next) {
+
+  try {
+    const { search: searchTerm, min_employees, max_employees } = req.body;
+    if (min_employees > max_employees) {
+      throw new ExpressError("min_employees cannot be greater than max_employees", 400);
+    }
+    const companies = await Companies.search(searchTerm, max_employees, min_employees);
+    return res.json({ companies });
+  }
+  catch (err) {
+    return next(err);
+  }
+});
+
+// create a new company, returns newly created => {company: companyData}
+router.post("/", async function (req, res, next) {
+  try {
+    const { handle, name, num_employees, description, logo_url } = req.body;
+    // ** POTENTIALLY HANDLE MISSING DATA
+    const newCompany = await Company.create(handle, name, num_employees, description, logo_url);
+    return res.json({ company: newCompany });
+  }
+  catch (err) {
+    return next(err);
+  }
+});
+
+/**  GET/companies/[handle]
+ get company by handle => return {company: companyData}
+*/
+router.get("/:handle", async function (req, res, next) {
+  try {
+    const handle = req.params.handle;
+    const company = await Company.get(handle);
+    if (!company) {
+      throw new ExpressError(`No such company: ${handle}`);
+    }
+    return res.json({ company });
+  }
+  catch (err) {
+    return next(err);
+  }
+});
+
+/** Updates an existing company by handle
+ * returns updated company info => return {company: companyData}
+ */
+router.patch("/:handle", async function (req, res, next) {
+  try {
+    const handle = req.params.handle;
+    const company = await Company.update(handle, req.body);
+    if (!company) {
+      throw new ExpressError(`No such company: ${handle}`);
+    }
+    return res.json({ company });
+  }
+  catch (err) {
+    return next(err);
+  }
 });
